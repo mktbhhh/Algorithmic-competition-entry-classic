e6e9832e906e1cfa26716de7c1b3d0e112006c0f
mktb <1270874562@qq.com>
2021-10-19 16:10:51 +0800

    UVa401

---
+++ ch3/UVa10082/UVa10082.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+char s[] = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+
+int main() {
+    int i, c;
+    while ((c = getchar()) != EOF) {
+        for (i = 0; s[i] && s[i] != c; i++);
+        if (s[i]) putchar(s[i - 1]);
+        else putchar(c);
+    }
+}
